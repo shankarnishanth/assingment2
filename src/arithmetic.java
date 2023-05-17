314d43a873bdc16e2eaab26adf3d90f5a1c29a23
shankarnishanth <nishanth23shankar@gmail.com>
2023-05-17 15:11:35 +0530

    added:arithmetic

---
+++ src/arithmetic.java
@@ -0,0 +1,12 @@
+public class arithmetic {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int c = 6;
+        System.out.println("a+b*c:" + a+b*c);
+        System.out.println("c+a/b:" + c+a/b);
+        System.out.println("a%b+c:" + a%b+c);
+        System.out.println("a*b+c:" + a*b+c);
+
+    }
+}
